3becd89d36710cdf71926bd04a385fb65c10e0b7
Saitama Sama <gourabnag12@gmail.com>
2020-08-30 14:58:46 +0530

    fix: color picker bug - misc: add ignore option to eslintrc

--- utils/rgb-to-hex.ts
+++ utils/rgb-to-hex.ts
@@ -1,7 +1,8 @@
 import Constants from './constants.json';
 
 export function componentToHex(c: number): string {
-  return c.toString(Constants.HEX_BASE);
+  const hex = c.toString(Constants.HEX_BASE);
+  return hex.length == 1 ? "0" + hex : hex;
 }
 
 export function rgbToHex(r: number, g: number, b: number): string {
